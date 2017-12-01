f20e7a6ddf2c11bb29a1d9a5659d7d29cab00c4e
xxh <760591483@qq.com>
2017-12-01 17:23:28 +0800

    去除多余部件

--- install.sh
+++ install.sh
@@ -26,60 +26,12 @@ mkdir ./libdir
 #-------------------------------
 cd ./src
 
-unzip ./cJSON-master.zip
-cd cJSON-master
-mkdir build
-cd ./build
-cmake ..
-if [ 0 -ne $?  ]
-then 
-        echo "cjson master cmake  error"
-        exit
-fi
-make
-if [ 0 -ne $?  ]
-then 
-        echo "cjson master make  error"
-        exit
-fi
-cp libcjson.so* $path/lib/
-cd $path/src/
-rm cJSON-master -rf
 
 
-unzip ./rabbitmq-c-master.zip
-cd rabbitmq-c-master
-mkdir build
-cd ./build
-cmake ..
-if [ 0 -ne $?  ]
-then 
-        echo "rabbitmq master cmake error"
-        exit
-fi
-make
-if [ 0 -ne $?  ]
-then 
-        echo "rabbitmq master make  error"
-        exit
-fi
-cd ./librabbitmq
-cp librabbitmq.so* $path/lib 
-cd $path/src
-rm rabbitmq-c-master -rf
-
-
-gcc -c -fpic cmccRabbitmq.c -I ../inc/
-gcc cmccRabbitmq.o -o libcmccRabbitmq.so -L ../lib/ -lrabbitmq -shared
-gcc -c -fpic cmccMysql.c -I ../inc/
-gcc cmccMysql.o -o libcmccMysql.so -L ../lib/ -lmysqlclient -lstdc++ -ldl -lpthread -lrt -shared
 gcc -c -fpic logLog.c -I ../inc/
 gcc logLog.o -o liblogLog.so -shared
 mv lib*.so ../lib/
 
-# gcc -c -fpic method.c -I ../inc/
-#gcc method.o -o libmethod.so -L ../lib/ -llogLog -lcmccRabbitmq -lcmccMysql -lcjson -shared
-#mv lib*.so ../lib/
 
 rm ./*.o -f 
 
